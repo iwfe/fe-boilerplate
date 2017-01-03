ab4e63e0bb2e83223c83d3ef593eb9fc11fa105b
slashhuang <627284482@qq.com>
2017-01-03 17:06:25 +0800

    revising layout of fe-boilerplate

--- build/webpack/plugin_loader.js
+++ build/webpack/plugin_loader.js
@@ -1,3 +1,16 @@
+// webpack 插件配置
+
+
+var webpack = require("webpack"),
+    CopyWebpackPlugin = require('copy-webpack-plugin'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    WebpackNotifierPlugin = require('webpack-notifier'),
+    HappyPack = require('happypack');
+
+var happyThreadPool = HappyPack.ThreadPool({ size: 2 });
+//切换打包方式为manifest，给后端读取json文件
+var ManifestPlugin = require('../plugins/manifest.js');
+
 var webpackPluginList =[
             new CopyWebpackPlugin([
                     {
@@ -59,18 +72,59 @@ var webpackPluginList =[
               threadPool: happyThreadPool,
               loaders: [ 'css-loader?sourceMap&-convertValues' ]
             }),
-            // new webpack.DllReferencePlugin({
-            //     context: __dirname,
-            //     manifest: require('./lib/manifest.json')
-            // })
-        ];
-if(versionType==2){
-    webpackPluginList.unshift(
-         new ManifestPlugin({
+            // manifest上传方式配置，这边先不用 versionType==2
+            process.env['NODE_ENV']=='manifest'? new ManifestPlugin({
                 hashNum:7,
                 extractJsCss:true,
                 versionDir:['html'] //递归抽取html下面的，也要加版本号
-    }));
+            }):null
+];
+
+var webpackLoaders = [
+            {
+                test: /\.js[x]?$/,
+                exclude: /(node_modules)|(global\/lib\/)/,
+                loader: 'babel-loader',
+                happy: { id: 'js' }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'happypack/loader?id=css')
+            },
+            {
+                test: /\.rcss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&sourceMap&-convertValues!sass-loader?sourceMap')
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?-convertValues!less-loader')
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap&-convertValues!sass-loader?sourceMap')
+            },
+            {
+                test: /\.(png|jpg|gif|woff|woff2|ttf|eot|svg|swf)$/,
+                loader: "file-loader?name=[name]_[sha512:hash:base64:7].[ext]"
+            },
+            {
+                test: /\.html/,
+                loader: "html-loader?" + JSON.stringify({
+                    minimize: false,
+                    attrs:false
+                })
+            },
+            {
+                 test: /\.json$/,
+                 loader: "json"
+            }
+];
+module.exports = {
+    plugins:webpackPluginList,
+    loaders:webpackLoaders
 }
 
-module.exports = webpackPluginList;
+
+
+
+
---
+++ build/webpack/webpack.config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by huangxiaogang on 17/1/3.
+ * webpack打包相关 引入babel-register
+ */
+require('babel-register');
+module.exports = require('./index');
